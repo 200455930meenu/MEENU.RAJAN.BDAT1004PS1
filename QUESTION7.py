89b28345d109e9ff0faa4add5876c621983cd2fd
200455930meenu <71960026+200455930meenu@users.noreply.github.com>
2020-10-02 22:41:18 +0530

    Add files via upload

---
+++ QUESTION7.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#add compatible variables
+6+'a'
+
+
+# In[4]:
+
+
+#index value not in the list
+a=[1,2,3,4,5,6,7,8,9,10]
+print (a[13])
+
+
+# In[6]:
+
+
+#A value out oft the range of functions input
+import math
+math.sqrt(-1.0)
+
+
+# In[7]:
+
+
+#value not defined
+x=values
+print(x)
+
+
+# In[ ]:
+
+
+
+
