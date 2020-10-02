6c2e2b2dff15b2b9b9560b6cc039a2d0397ed199
200455930meenu <71960026+200455930meenu@users.noreply.github.com>
2020-10-02 22:43:11 +0530

    Add files via upload

---
+++ QUESTION10.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def sieve(n):
+ int a=2
+ n=int(input('enter a number: '))
+ for s in range(a,n+1):
+    if n>2:
+        for x in range(a,s)
+          if(s%2)==0:
+                print('not possible')
+          
+          else:
+                  print(n)  
+
