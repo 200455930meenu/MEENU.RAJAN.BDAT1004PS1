5191c3d86b925187ab1223c00a2c5dccbdebac26
200455930meenu <71960026+200455930meenu@users.noreply.github.com>
2020-10-02 21:41:23 +0530

    Add files via upload

---
+++ QUESTION4.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#QUESTION4
+word = input ("enter a word: ")
+    
+
+
+# In[ ]:
+
+
+def pig(word):
+    for i in range (word):
+        
+        if(word[0]='a' or 'e' or 'i' or 'u'):
+        
+           print (a+'way')
+            
+        else:
+            
+            print (word[1:]+'ay')
+
+
+# In[ ]:
+
+
+
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
